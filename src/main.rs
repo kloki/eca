f54da8963a0b1956605e2616b7dff889b1d656c7
Koen Klinkers <k.klinkers@gmail.com>
2025-05-18 22:02:02 +0200

    automates file names

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ struct Args {
     /// Rule set to be applied
     rule_set: u8,
     /// Name of output file
-    #[arg(short, long, default_value = "./output.png")]
-    output_file: String,
+    #[arg(short, long)]
+    output_file: Option<String>,
     //width of initalization
     #[arg(long, default_value_t = 800)]
     width: u32,
@@ -23,17 +23,26 @@ struct Args {
     initalization: Init,
 }
 
+impl Args {
+    fn get_output_file(&self) -> String {
+        match &self.output_file {
+            Some(s) => s.to_string(),
+            None => format!("{}.png", self.rule_set),
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
     let rs = RuleSet::new(args.rule_set);
     let width = args.width as usize;
     let iterations = args.iterations as usize;
+    let file_name = args.get_output_file();
     let mut eca = Eca::new(width, args.initalization, rs);
     eca.iterate(iterations);
-
-    if args.output_file.ends_with(".png") {
-        writer::to_png(&args.output_file, eca.iterations).expect("failed to write image");
+    if file_name.ends_with(".png") {
+        writer::to_png(&file_name, eca.iterations).expect("failed to write image");
     } else {
-        writer::to_txt(&args.output_file, eca.iterations).expect("failed to write to txt file")
+        writer::to_txt(&file_name, eca.iterations).expect("failed to write to txt file")
     }
 }
